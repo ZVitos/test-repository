8baaab9e57880c493546448f827ff018af5b1403
ZVitos <138370078+ZVitos@users.noreply.github.com>
2024-11-13 22:53:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Дубикова Виталия Федоровича - Lab_5";
+  const greeting = "Это тестовый проект ФИО";
 
   console.log('This is App ');
   return (
