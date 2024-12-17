2a390228c539c7fbb2d38ae41fc9c72e5a359254
ZVitos <138370078+ZVitos@users.noreply.github.com>
2024-12-17 16:25:56 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Дубикова Виталия Федоровича - commit chore";
+  const greeting = "Это тестовый проект Зайцевой М.М.";
 
   return (
     <div className="App">
